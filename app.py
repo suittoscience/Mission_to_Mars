3fe6ace8e64295a464573a7b906de8c72dd363af
suitto321 <suittoscience@gmail.com>
2018-12-20 13:43:07 -0500

    update

--- app.py
+++ app.py
@@ -2,21 +2,17 @@
 from flask import Flask, render_template, redirect 
 from flask_pymongo import PyMongo
 import scrape_mars
-import os
-
 
 # Hidden authetication file
-#import config 
+# import config 
 
 # Create an instance of Flask app
 app = Flask(__name__)
 
-#Use flask_pymongo to set up connection 
-
+#Use pymongo to set up connection 
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-# Use flask_pymongo to set up mongo connection locally 
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-# mongo = PyMongo(app)
+ 
+
 
 # Create route that renders index.html template and finds documents from mongo
 @app.route("/")
